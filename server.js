32e9ccc314fc6d4a50bfa4a8a668ab62fcedb84f
Stephen Kolbo <sskolbo88@yahoo.com>
2022-11-03 01:01:27 -0500

    Removing compression to debug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+//const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
+//app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
